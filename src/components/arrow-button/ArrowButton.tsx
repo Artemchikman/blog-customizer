8c2ee51191fb1a8c72219fa71647e8057f78fb7b
Artem <artem-10.98@yandex.ru>
2024-08-06 13:25:28 +0300

    Отправка-1.1.2

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -1,19 +1,31 @@
 import arrow from 'src/images/arrow.svg';
-
 import styles from './ArrowButton.module.scss';
+import clsx from 'clsx';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
 
-export const ArrowButton = () => {
+// Объявление типа Props для ArrowButton
+export type ArrowButtonProps = {
+	onClick: OnClick; // callback для обработки клика
+	isOpen: boolean; // состояние открытия/закрытия
+};
+
+export const ArrowButton = (props: ArrowButtonProps) => {
 	return (
-		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={clsx(styles.container, {
+				[styles.container_open]: props.isOpen,
+			})}
+			onClick={props.onClick}>
+			<img
+				src={arrow}
+				alt='иконка стрелочки'
+				className={clsx(styles.arrow, { [styles.arrow_open]: props.isOpen })}
+			/>
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,17 +1,142 @@
+import { useState, useEffect, useRef } from 'react';
 import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
-
+import { RadioGroup } from 'components/radio-group';
+import { Select } from 'components/select';
 import styles from './ArticleParamsForm.module.scss';
+import { Text } from '../text';
+import {
+	fontFamilyOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+	fontSizeOptions,
+	defaultArticleState,
+	ArticleStateType,
+} from 'src/constants/articleProps';
 
-export const ArticleParamsForm = () => {
+// Определяем типы для пропсов компонента ArticleParamsForm.
+type ArticleParamsFormProps = {
+	onApply: (newState: ArticleStateType) => void; // Функция обработки изменения состояния статьи
+};
+
+// Основной компонент ArticleParamsForm
+export const ArticleParamsForm = ({ onApply }: ArticleParamsFormProps) => {
+	// Инициализируем состояния компонента:
+	const [isOpen, setIsOpen] = useState(false); // Состояние для управления открытием/закрытием сайдбара
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState); // Состояние формы, инициализируем значениями по умолчанию
+	const sidebarRef = useRef<HTMLDivElement>(null); // Хук useRef для ссылки на элемент сайдбара
+
+	// Функция для переключения состояния сайдбара.
+	const toggleOpen = () => {
+		setIsOpen(!isOpen); // Меняем состояние isOpen на противоположное
+	};
+
+	// Функция для сброса состояния формы до значений по умолчанию.
+	const resetForm = () => {
+		setFormState(defaultArticleState); // Устанавливаем состояние на значения по умолчанию
+	};
+
+	// Функция для применения изменений, полученных из формы.
+	const applyChanges = (event: React.FormEvent) => {
+		event.preventDefault(); // Предотвращаем стандартное поведение формы (перезагрузку страницы)
+		onApply(formState); // Вызываем функцию обработки с новыми значениями состояния
+		setIsOpen(false); // Закрываем сайдбар после применения изменений
+	};
+
+	// Функция для обновления состояния формы при изменении значений.
+	const handleChange = <K extends keyof ArticleStateType>(
+		key: K,
+		value: ArticleStateType[K]
+	) => {
+		setFormState((prevState) => ({
+			...prevState, // Сохраняем предыдущее состояние
+			[key]: value, // Обновляем конкретное значение состояния
+		}));
+	};
+
+	// Эффект для закрытия сайдбара при клике вне его области или нажатии клавиши Escape.
+	useEffect(() => {
+		const handleClickOutside = (event: MouseEvent) => {
+			// Проверяем, был ли клик вне области сайдбара
+			if (
+				sidebarRef.current &&
+				!sidebarRef.current.contains(event.target as Node)
+			) {
+				setIsOpen(false); // Если да, закрываем сайдбар
+			}
+		};
+
+		const handleKeyDown = (event: KeyboardEvent) => {
+			if (event.key === 'Escape') {
+				setIsOpen(false); // Закрываем при нажатии Escape
+			}
+		};
+
+		// Добавляем обработчики событий для кликов и нажатий клавиш
+		document.addEventListener('mousedown', handleClickOutside);
+		document.addEventListener('keydown', handleKeyDown);
+
+		// Функция очистки, когда компонент размонтируется
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside);
+			document.removeEventListener('keydown', handleKeyDown);
+		};
+	}, []); // Эффект выполняется только при монтировании и размонтировании компонента
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton onClick={toggleOpen} isOpen={isOpen} />
+			{/* Кнопка для открытия/закрытия сайдбара */}
+			<aside
+				className={`${styles.container} ${isOpen ? styles.container_open : ''}`}
+				ref={sidebarRef}>
+				{/* Сайдбар с формой параметров статьи */}
+				<form className={styles.form} onSubmit={applyChanges}>
+					<Text as={'h2'} size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						options={fontFamilyOptions} // Опции шрифтов
+						selected={formState.fontFamilyOption} // Выбранный шрифт
+						onChange={(option) => handleChange('fontFamilyOption', option)} // Обработка изменения шрифта
+						placeholder='Шрифт' // Плейсхолдер
+						title='Шрифт' // Название выбора
+					/>
+					<RadioGroup
+						name='fontSize' // Имя группы радиокнопок
+						options={fontSizeOptions} // Опции размеров шрифта
+						selected={formState.fontSizeOption} // Выбранный размер шрифта
+						onChange={(option) => handleChange('fontSizeOption', option)} // Обработка изменения размера шрифта
+						title='Размер шрифта' // Название группы
+					/>
+					<Select
+						options={fontColors} // Опции цветов шрифта
+						selected={formState.fontColor} // Выбранный цвет шрифта
+						onChange={(option) => handleChange('fontColor', option)} // Обработка изменения цвета шрифта
+						placeholder='Цвет шрифта' // Плейсхолдер
+						title='Цвет шрифта' // Название выбора
+					/>
+					<Select
+						options={backgroundColors} // Опции цветов фона
+						selected={formState.backgroundColor} // Выбранный цвет фона
+						onChange={(option) => handleChange('backgroundColor', option)} // Обработка изменения цвета фона
+						placeholder='Цвет фона' // Плейсхолдер
+						title='Цвет фона' // Название выбора
+					/>
+					<Select
+						options={contentWidthArr} // Опции ширины контента
+						selected={formState.contentWidth} // Выбранная ширина контента
+						onChange={(option) => handleChange('contentWidth', option)} // Обработка изменения ширины контента
+						placeholder='Ширина контента' // Плейсхолдер
+						title='Ширина контента' // Название выбора
+					/>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' type='reset' />
-						<Button title='Применить' type='submit' />
+						<Button title='Сбросить' type='reset' onClick={resetForm} />{' '}
+						{/* Кнопка для сброса параметров */}
+						<Button title='Применить' type='submit' />{' '}
+						{/* Кнопка для применения параметров */}
 					</div>
 				</form>
 			</aside>
